e93fffab4e3e6b1c7da3bb91395e6fe98d4f314c
MacAir300 <mac300@macbook-air-macair300.local>
2019-04-18 21:28:52 +0300

    constreins

--- table cell/ViewController.swift
+++ table cell/ViewController.swift
@@ -19,17 +19,25 @@ class ViewController:
     @IBOutlet weak var myTableView: UITableView!
     @IBOutlet weak var myLabelGo: UILabel!
     
+    
+    
     var arrayName:[String] = []
+    var colorConst : Int = 3                                                        // Const Number of colors
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
         
         myTableView.tableFooterView = UIView()
-        myTableView.keyboardDismissMode = UIScrollView.KeyboardDismissMode.onDrag   // hide keyboard
+        myTableView.keyboardDismissMode = UIScrollView.KeyboardDismissMode.onDrag    // Hide keyboard
+        
+
+        myTableView.rowHeight = UITableView.automaticDimension
+        myTableView.estimatedRowHeight = 600
         
     }
 
+    
     @IBAction func addButtonText(_ sender: AnyObject) {
         myLabelGo.text = "Hello " + myTextField.text!
         arrayName.append(myTextField.text!)
@@ -38,11 +46,22 @@ class ViewController:
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayName.count
-    }
+        }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
         cell.textLabel?.text = arrayName[indexPath.row]
+            let colorVari = indexPath.row                                               // Color analysis unit
+            if (colorVari%colorConst)==0 {
+                cell.backgroundColor = UIColor.blue
+            } else if (colorVari%colorConst)==1 {
+                cell.backgroundColor = UIColor.red
+            } else if (colorVari%colorConst)==2 {
+                cell.backgroundColor = UIColor.green
+            }
+        
+        
         return cell
     }
+
 }
